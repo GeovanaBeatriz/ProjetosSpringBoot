f967f16fb43dd05cbf34327993fcec87a8231404
GeovanaBeatriz <geovana.jesus.184945@gmail.com>
2023-03-14 13:31:58 -0300

    Adding files

---
+++ course/src/main/java/com/geovanabeatriz/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.geovanabeatriz.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //Caracterizando um projeto Spring
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
---
+++ course/src/test/java/com/geovanabeatriz/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geovanabeatriz.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
