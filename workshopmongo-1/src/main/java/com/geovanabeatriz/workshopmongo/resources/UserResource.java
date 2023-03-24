1ad740445378fe604b9f0c2d483a03393a4210f2
GeovanaBeatriz <geovana.jesus.184945@gmail.com>
2023-03-24 20:18:41 -0300

    Obtendo um post por id

--- workshopmongo-1/src/main/java/com/geovanabeatriz/workshopmongo/resources/UserResource.java
+++ workshopmongo-1/src/main/java/com/geovanabeatriz/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.geovanabeatriz.workshopmongo.domain.Post;
 import com.geovanabeatriz.workshopmongo.domain.User;
 import com.geovanabeatriz.workshopmongo.dto.UserDTO;
 import com.geovanabeatriz.workshopmongo.services.UserService;
@@ -67,6 +68,13 @@ public class UserResource {
 	obj.setId(id);
 	obj = service.insert(obj);
 	return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity <List<Post>> findPosts(@PathVariable String id){
+		
+	User obj = service.findById(id);	
+	return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 }
---
+++ workshopmongo-1/src/main/java/com/geovanabeatriz/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.geovanabeatriz.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.geovanabeatriz.workshopmongo.domain.Post;
+import com.geovanabeatriz.workshopmongo.repositories.PostRepository;
+import com.geovanabeatriz.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Post user = repo.findOne(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+	
+	
+}
