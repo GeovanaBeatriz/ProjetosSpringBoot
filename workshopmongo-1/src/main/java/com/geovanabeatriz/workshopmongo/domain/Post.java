988b375eea1b48f137ce7273344c70a52dcd4b60
GeovanaBeatriz <geovana.jesus.184945@gmail.com>
2023-03-24 21:40:15 -0300

    Query Methods

--- workshopmongo-1/src/main/java/com/geovanabeatriz/workshopmongo/domain/Post.java
+++ workshopmongo-1/src/main/java/com/geovanabeatriz/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.geovanabeatriz.workshopmongo.domain;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.geovanabeatriz.workshopmongo.dto.AuthorDTO;
+import com.geovanabeatriz.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,7 @@ public class Post implements Serializable {
 	private String body;
 	private AuthorDTO author;
 	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -70,6 +74,14 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 
 	@Override
@@ -96,4 +108,6 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 }
